f1c922f76e957df85dc070c23f951fa8e6f094a3
Bahram Rousta <98008843+BahramRousta@users.noreply.github.com>
2023-03-18 14:20:57 +0330

    add todo model tdd test

---
+++ todo/tests/test_model.py
@@ -0,0 +1,13 @@
+import pytest
+from ..models import Todo
+
+
+class TestModel:
+
+    def test_should_be_create_instance(self, db):
+        Todo.objects.create(name="something")
+        assert Todo.objects.count() == 1
+
+    def test_should_repr_instance(self, db):
+        todo = Todo.objects.create(name="something")
+        assert str(todo) == "something"
